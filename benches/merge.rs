2968697bae39e6a689196c9af8c587a54cef3b0c
Maiko Mueller <Maiko.Muller@grenoble-inp.org>
2019-12-17 13:06:33 +0100

    Fix benches

--- benches/merge.rs
+++ benches/merge.rs
@@ -3,7 +3,7 @@ use itertools::kmerge;
 use rayon::prelude::*;
 use rayon::{join, ThreadPoolBuilder};
 use task_splitting::*;
-const N: usize = 100000;
+const N: usize = 1000000;
 
 /*
  * Split single-threaded in N parts and them merg them
@@ -17,11 +17,15 @@ fn merge_n_bench(c: &mut Criterion) {
     let mut group = c.benchmark_group("merge_n");
     for size in [1, 2, 3, 4, 5, 6, 7, 8].iter() {
         group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
-            b.iter(|| {
-                let mut x = v.clone(); // we shouldn't re-use that thing
-                merge_n(black_box(&mut x), &mut buffer, size);
-                assert_eq!(checksum, x.iter().sum::<u64>());
-            });
+            b.iter_batched(
+                || v.clone(),
+                |mut v| {
+                    merge_n(black_box(&mut v), &mut buffer, size);
+                    //assert_eq!(checksum, v.iter().sum::<u64>());
+                    v
+                },
+                BatchSize::SmallInput,
+            );
         });
     }
     group.finish();
@@ -47,13 +51,17 @@ fn parallel_merge_n_bench(c: &mut Criterion) {
         let levels = (split as f64).log(*size as f64).ceil() as u64;
         println!("Need {} recursive splits for {}", levels, size);
         group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
-            b.iter(|| {
-                pool.install(|| {
-                    let mut x = v.clone(); // we shouldn't re-use that thing
-                    parallel_merge_n(black_box(&mut x), &mut buffer, size, levels);
-                    assert_eq!(checksum, x.iter().sum::<u64>());
-                });
-            });
+            b.iter_batched(
+                || v.clone(),
+                |mut v| {
+                    pool.install(|| {
+                        parallel_merge_n(black_box(&mut v), &mut buffer, size, levels);
+                        assert_eq!(checksum, v.iter().sum::<u64>());
+                    });
+                    v
+                },
+                BatchSize::SmallInput,
+            );
         });
     }
     group.finish();
@@ -66,22 +74,30 @@ fn merge_kmerge(c: &mut Criterion) {
         .collect();
     let checksum: u64 = v.iter().sum();
     c.bench_function("kmerge", |b| {
-        b.iter(|| {
-            let mut x = v.clone(); // we shouldn't re-use that thing
-            merge_n(black_box(&mut x), &mut buffer, 2);
-            assert_eq!(checksum, x.iter().sum::<u64>());
-        })
+        b.iter_batched(
+            || v.clone(),
+            |mut v| {
+                merge_n(black_box(&mut v), &mut buffer, 2);
+                // assert_eq!(checksum, v.iter().sum::<u64>());
+                v
+            },
+            BatchSize::SmallInput,
+        )
     });
     c.bench_function("merge", |b| {
-        b.iter(|| {
-            let mut x = v.clone(); // we shouldn't re-use that thing
-            merge_2(black_box(&mut x), &mut buffer);
-            assert_eq!(checksum, x.iter().sum::<u64>());
-        })
+        b.iter_batched(
+            || v.clone(),
+            |mut v| {
+                merge_2(black_box(&mut v), &mut buffer);
+                // assert_eq!(checksum, v.iter().sum::<u64>());
+                v
+            },
+            BatchSize::SmallInput,
+        )
     });
 }
 
-//criterion_group!(benches, merge_kmerge);
-criterion_group!(benches, parallel_merge_n_bench);
+criterion_group!(benches, merge_kmerge);
+// criterion_group!(benches, parallel_merge_n_bench);
 // criterion_group!(benches, parallel_merge_n_bench, merge_n_bench);
 criterion_main!(benches);
